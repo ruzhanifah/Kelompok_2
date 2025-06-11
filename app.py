593ab087d6d89e7acffdf8a10995ac0602c05513
ruzhanifah <ruzhanifah@gmail.com>
2025-06-11 10:15:43 +0700

    app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="Sistem Rekomendasi Lagu", layout="wide")
+
+st.title("🎧 Sistem Rekomendasi Lagu")
+st.write("""
+Selamat datang di dashboard rekomendasi lagu berbasis data mining!  
+Silakan gunakan sidebar kiri untuk menjelajahi halaman:
+- EDA & Dataset
+- Model & Clustering
+- Form Rekomendasi
+""")
